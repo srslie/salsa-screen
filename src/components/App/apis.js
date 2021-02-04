dd599684952cbacf32c1afa02c96e93da25d131b
srslie <aliceruppert@gmail.com>
2021-02-04 12:36:56 -0700

    Fix typo

--- src/components/App/apis.js
+++ src/components/App/apis.js
@@ -7,7 +7,7 @@ const apis = {
 
   //returns All movies in database with average rating:{"movies": [{id: 1, title: "Movie Title", poster_path: "someURL", backdrop_path: "someURL", release_date: "2019-12-04", overview: "Some overview", average_rating: 6 }, ...]}
   getMovies() {
-    return this.getData('/moves')
+    return this.getData('/movies')
   },
 
   //returns The movie corresponding to the id sent in the URL: {"movie": {id: 1, title: "Movie Title", poster_path: "someURL", backdrop_path: "someURL", release_date: "2019-12-04", overview: "Some overview", average_rating: 6, genres: [{id: 18, name:"Drama"}], budget:63000000, revenue:100853753, runtime:139, tagline: "Movie Tagline" }}
