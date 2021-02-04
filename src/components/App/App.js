9f0f7549c1e1251a45c5b81bc61f307f28ecff90
srslie <aliceruppert@gmail.com>
2021-02-04 12:34:22 -0700

    Add componentDidMount with api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,34 @@
-import React, { Component } from 'react';
-import './App.css';
-import movieData from '../../data/movieData';
+import React, { Component } from 'react'
+import './App.css'
+import movieData from '../../data/movieData'
 import Movies from '../Movies/Movies'
 import SearchBar from '../SearchBar/SearchBar'
+import apis from './apis'
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: movieData.movies
+      movies: [],
+      error: '',
+      loading: false
     }
   }
+
+  componentDidMount() {
+    apis.getMovies()
+      .then(data => {
+        this.setState({
+          movies: data.movies, 
+          loading: true})
+      })
+      .catch(error => {
+        this.setState({
+          error: 'Sorry we\'re having trouble loading themovies, have some chips!', 
+        })
+      })
+  }   
 
   showSearchResults = results => {
     this.setState({movies: results})
@@ -26,6 +43,16 @@ class App extends Component {
           <button>Forward</button>
         </header>
         <SearchBar className="searchbar" movies={this.state.movies} showSearchResults={this.showSearchResults}/>
+        {this.state.loading &&
+          <div className="loading">
+            <p>Loading...</p>
+          </div> 
+        }
+        { !this.state.movies.length && 
+            <div className="errorLoading">
+              <p>{this.state.error}</p>
+            </div> 
+        } 
         <Movies className="movies" movies={this.state.movies} />
         <footer>
           <p className="copyright">© srslie - 2021</p>
