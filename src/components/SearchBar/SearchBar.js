ffc7687df63923dbeb3976888ffe2644c486593c
srslie <aliceruppert@gmail.com>
2021-02-05 03:50:43 -0700

    Refactor search for display and faster response

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,29 +1,26 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  constructor(props) {
+  constructor() {
     super();
     this.state = {
-      searchInput: '',
-      movieList: props.movies,
-      search: props.showSearchResults
+      searchInput: ''
     }
     
   }
 
   handleChange = event => {
     event.preventDefault()
+    this.setState({searchInput: event.target.value.toLowerCase()})
 
-    this.setState({searchInput: event.target.value.toLowerCase() })
-
-    const titleMatch = this.state.movieList.filter(movie => {
+    const titleMatch = this.props.movies.filter(movie => {
       const movieTitle = movie.title.toLowerCase()
-      return movieTitle.includes(this.state.searchInput)
+      return movieTitle.includes(event.target.value.toLowerCase())
     })
     // const descriptionMatch = this.movieList.filter(movie => movie.description.includes(this.searchInput))
     // const searchResults = [...titleMatch, ...descriptionMatch]
     // const results = titleMatch.length ? titleMatch : '<p>Sorry, no results matched your search.<p>'
-    this.state.search(titleMatch)
+    this.props.showSearchResults(titleMatch)
   }
 
   render() {
