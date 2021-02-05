88bbe5b5403603a7e1d8ca7572b69dd51c0fb798
srslie <aliceruppert@gmail.com>
2021-02-05 03:47:01 -0700

    Add button and method to show a SelectedCard

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './card.css'
+import SelectedMovie from '../SelectedMovie/SelectedMovie'
 
-const Card = ({id, poster_path, title, average_rating, release_date}) => {
+const Card = movie => {
+
   const convertDate = date => {
     const dateSplit = date.split('-')
     const dateJoined =  dateSplit.join(',')
@@ -11,13 +13,21 @@ const Card = ({id, poster_path, title, average_rating, release_date}) => {
     return monthYear.join(' ')
   }
 
+  const showMoreInfo = event => {
+    event.preventDefault()
+    return (
+      <SelectedMovie movie={movie}/>
+    )
+  }
+
   return (
-    <div className="card" id={id} key={id}>
-      <img src={poster_path} alt="movie poster"/>
+    <div className="card" id={movie.id} key={movie.id}>
+      <img src={movie.poster_path} alt="movie poster"/>
       <div className="info">
-        <h1 className="title">{title}</h1>
-        <h2 className="date">{convertDate(release_date)}</h2>
-        <h2 className="rating">{average_rating.toFixed(2)}</h2>
+        <h1 className="title">{movie.title}</h1>
+        <h2 className="date">{convertDate(movie.release_date)}</h2>
+        <h2 className="rating">{movie.average_rating.toFixed(2)}</h2>
+        <button onClick={showMoreInfo}>More Info</button>
       </div>
     </div>
   )
