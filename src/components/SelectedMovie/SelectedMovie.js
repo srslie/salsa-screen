39563dbc26fa6966c2d910ac75f9dddda30892ad
srslie <aliceruppert@gmail.com>
2021-02-06 13:46:09 -0700

    Add basic info jsx to selectedMovie

--- src/components/SelectedMovie/SelectedMovie.js
+++ src/components/SelectedMovie/SelectedMovie.js
@@ -1,41 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './selectedMovie.css'
-import apis from '../../apis'
 
-class SelectedMovie extends Component {
-  constructor(props) {
-    super();
-    this.state = {
-      input: ''
-    }
-  }
-
-  componentDidMount() {
-    
-  }
-
-  handleSubmit = event => {
-    event.preventDefault()
-
-    Promise.resolve(apis.postUserRating(1, this.props.movie.id, this.state.input))
-      .then(() => {
-        Promise.resolve(apis.getUserRatings())
-      })
-  }
-
-  render() {
+const SelectedMovie = ({movie, displayAllMovies, convertDate}) => {
     return (
-      <div className="selectedMovie">
-        <button className="exit" onClick={}>X</button>
-        <h3 className="overview">{this.props.overview}</h3>
-        <form>
-          <label htmlFor="rating">Rate Movie!</label>
-          <input className="rating" type="number" value='userRating' placeholder='10' />
-          <button className="rate" onClick={this.handleSubmit}></button>
-        </form>
-      </div>
+      <div className="selectedMovie" style={{ 
+        backgroundImage: `url(${movie.backdrop_path})` 
+      }}>
+        <button className="exit" onClick={displayAllMovies}>X</button>
+        <img src={movie.poster_path} alt="movie poster"/>
+        <div className="info">
+          <h1 className="title">{movie.title}</h1>
+          <h2 className="rating">Rating: {movie.average_rating.toFixed(2)}</h2>
+          <h2 className="date">Released: {convertDate(movie.release_date)}</h2>
+          <h3 className="tagline">{movie.tagline}</h3>
+          <h3 className="overview">Overview: {movie.overview}</h3>
+          <div className="genres">
+            <h3>Genres:</h3>
+            <ul className="genresList">
+              {movie.genres.map(genre => <li>genre</li>)}
+            </ul>  
+          </div>
+          <h3 className="budget">{movie.budget}</h3>
+          <h3 className="revenue">{movie.revenue}</h3>
+          <h3 className="runtime">{movie.runtime}</h3>
+          <div className="videos">
+            <h3>Genres:</h3>
+            <ul className="videosList">
+              {movie.videos.map(video => {
+                  return <p>video</p>
+                })
+              }
+            </ul>  
+          </div>
+        </div>
+      </div>  
     )
-  }
 }
 
 export default SelectedMovie;
