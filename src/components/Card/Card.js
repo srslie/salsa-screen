76931e762f5dde14d2d408c0f592ad254c0b3edb
srslie <aliceruppert@gmail.com>
2021-02-07 02:07:54 -0700

    Simplify onclick function

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,11 +3,6 @@ import './card.css'
 
 const Card = (movie, showSelectedMovie, convertDate) => {
 
-  const showMoreInfo = event => {
-    event.preventDefault()
-    showSelectedMovie(movie)
-  }
-
   return (
     <div className="card" id={movie.id} key={movie.id}>
       <img src={movie.poster_path} alt="movie poster"/>
@@ -15,7 +10,7 @@ const Card = (movie, showSelectedMovie, convertDate) => {
         <h1 className="title">{movie.title}</h1>
         <h2 className="date">{convertDate(movie.release_date)}</h2>
         <h2 className="rating">{movie.average_rating.toFixed(2)}</h2>
-        <button onClick={showMoreInfo}>More Info</button>
+        <button onClick={() => showSelectedMovie(movie)}>More Info</button>
       </div>
     </div>
   )
