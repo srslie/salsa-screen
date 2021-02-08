0209741c4132e4cdb84d4d18321594822c4b77ae
srslie <aliceruppert@gmail.com>
2021-02-08 15:15:00 -0700

    Break out Error into component

---
+++ src/components/Error/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './error.css'
+
+const Error = () => {
+  return (
+    <div className="error">
+      <p>Error! Try Reloading.</p>
+    </div> 
+  )
+}
+
+export default Error;
