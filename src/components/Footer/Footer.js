f2fc2e7a4327ee215987dcbd7b2f048cf2831014
srslie <aliceruppert@gmail.com>
2021-02-08 15:41:58 -0700

    Fix typo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import './selectedMovie.css';
+import './footer.css';
 
 const Footer = () => {
   return (
     <footer>
       <p className="copyright">© srslie - 2021</p>
     </footer>
-
   )
-
 }
 
 export default Footer;
