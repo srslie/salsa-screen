e28d10efa0ae9959b07afdc9041036bf365738a6
srslie <aliceruppert@gmail.com>
2021-02-07 18:57:21 -0700

    Add onclick to card image instead of button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,12 +5,11 @@ const Card = (movie, showSelectedMovie) => {
 
   return (
     <div className="card" id={movie.id} key={movie.id}>
-      <img src={movie.poster_path} alt="movie poster"/>
+      <img src={movie.poster_path} alt="movie poster" onClick={() => showSelectedMovie(movie)} />
       <div className="info">
-        <h1 className="title">{movie.title}</h1>
-        <h2 className="date">{movie.release_date}</h2>
-        <h2 className="rating">{movie.average_rating}</h2>
-        <button onClick={() => showSelectedMovie(movie)}>More Info</button>
+        <h1 className="card-title">{movie.title}</h1>
+        <h2 className="card-date">{movie.release_date}</h2>
+        <h2 className="card-rating">🌶 {movie.average_rating}🌶</h2>
       </div>
     </div>
   )
