fe91a223c971c2c1fe5337e6db4b0b62c3a347b2
srslie <aliceruppert@gmail.com>
2021-02-08 15:14:47 -0700

    Break out Footer into component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './selectedMovie.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="copyright">© srslie - 2021</p>
+    </footer>
+
+  )
+
+}
+
+export default Footer;
