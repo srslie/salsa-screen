f56d8a05dbd603806ce34f63bd188da5f859c072
srslie <aliceruppert@gmail.com>
2021-02-10 00:52:42 -0700

    Return component instead of function call

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import './movies.css'
 import Card from '../Card/Card'
-import apis from '../../apis'
 
-const Movies = ({movies, searchResults, showSelectedMovie, convertDate}) => {
-
-  console.log(movies)
+const Movies = ({movies, searchResults}) => {
   const moviesToDisplay = searchResults.length ? searchResults : movies
   return (
-    <div className="movies">
-      {moviesToDisplay.map(movie => {
-        return Card(movie, showSelectedMovie, convertDate)
-      })}
-    </div>
+      <div className="movies">
+        {moviesToDisplay.map(movie => <Card movie={movie} />)}
+      </div>
   )
 }
 
