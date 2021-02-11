a2ddb2b8d89a70d8cf3ce3168456e7c605e23a46
srslie <aliceruppert@gmail.com>
2021-02-11 10:21:03 -0700

    Start genre menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import './header.css';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Button';
+import MenuItem from '@material-ui/core/Button';
+import anchorEl from '@material-ui/core/Button';
+import open from '@material-ui/core/Button';
+import Fade from '@material-ui/core/Button';
 
 const Header = () => {
+  const handleClick = () => {
+
+  }
+
+  const handleClose = () => {
+
+  }
+
   return (
     <header>
-      <button>{'<'}</button>
       <h1>🌶 Salsa Screen 🎬</h1>
-      <button>{'>'}</button>
+      <Button className="genreButton" aria-controls="fade-menu" aria-haspopup="true" onClick={handleClick}>
+      Genres
+      </Button>
+      <Menu className="genreMenu"
+        id="fade-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={handleClose}>Action</MenuItem>
+        <MenuItem onClick={handleClose}>Adventure</MenuItem>
+        <MenuItem onClick={handleClose}>Comedy</MenuItem>
+        <MenuItem onClick={handleClose}>Comedy</MenuItem>
+      </Menu>
     </header>
   )
 
