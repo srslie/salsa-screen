fe95721710a17bc78d8d65401a58a15c194349df
srslie <aliceruppert@gmail.com>
2021-02-12 20:54:51 -0700

    Add error icon

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import './error.css'
+import './error.css';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';;
 
 const Error = ({error}) => {
   return (
     <div className="error">
+      <ErrorOutlineIcon className='icon' />
       <p>{error}</p>
     </div> 
   )
