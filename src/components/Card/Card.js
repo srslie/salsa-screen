39592fa0616449564bb97aa15d5e5ba54d086eb7
srslie <aliceruppert@gmail.com>
2021-02-12 20:53:46 -0700

    Add card styling hover and tooltip

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import './card.css'
-import {Link} from 'react-router-dom'
+import './card.css';
+import {Link} from 'react-router-dom';
+import ToolTip from '@material-ui/core/Tooltip';
 
 const Card = ({movie}) => {
   return (
+    <ToolTip title={`Click for details about ${movie.title} 🎥 `}arrow>
     <Link to={`/movie/${movie.id}`}>
-      <div className="card" id={movie.id} key={movie.id}>
-        <img src={movie.poster_path} alt="movie poster" />
-        <div className="info">
-          <h1 className="card-title">{movie.title}</h1>
-          <h2 className="card-date">{movie.release_date}</h2>
-          <h2 className="card-rating">🌶 {movie.average_rating}</h2>
+      <div className="card" 
+        id={movie.id} 
+        key={movie.id}
+      >
+        <img className="cardImg" 
+          src={movie.poster_path} 
+          alt={`${movie.title} movie poster from ${movie.release_date}`} 
+        />
+        <div className="cardInfo hidden">
+          <span className="ratingOverlay">
+            <h2 className="crd-rating">🌶 {movie.average_rating}%</h2>
+          </span>
         </div>
       </div>
     </Link>
+    </ToolTip>
   )
 }
 
