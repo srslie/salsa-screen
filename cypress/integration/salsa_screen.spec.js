c3a2ace128e7e5dc4115bbf7d94be8cc4d9ce3af
srslie <aliceruppert@gmail.com>
2021-02-16 20:00:32 -0700

    Finish tests for browse and select display

--- cypress/integration/salsa_screen.spec.js
+++ cypress/integration/salsa_screen.spec.js
@@ -29,18 +29,29 @@ describe('Salsa Screen', () => {
     cy.visit('http://localhost:3000')
   }); 
 
-  it('should always show a header', () => {
-    cy.get('header').children('a', '.menu-wrapper', '.search-bar')  
+  it('should always show a header with home link', () => {
+    cy.get('header').children('a', '.menu-wrapper', '.search-bar') 
+      .get('a').children('img')
+      .get('.site-name').click()
+    cy.url().should('contain', '/')
   });
 
+  it('should show browse menu and display selected genre', () => {
+    cy.get('header').children('.menu-wrapper')
+      .get('.menu-wrapper').children('button').click()
+    cy.get('ul').children('button')
+      .get('.Action').click({multiple: true})
+      cy.wait(5)
+    cy.url().should('include', 'searchResults')
+  })
+
   it('should show a footer', () => {
-    cy.get('footer').children('nav').children('ol')
+    cy.get('footer').children('nav').children('ol').children('li')
   });
   
   it('should show a loading page until movies data is loaded', () => {
     cy.clock()
-    cy
-      .get('.loading').children('p')
+    cy.get('.loading').children('p')
       .get('p').contains('Loading...')
   });
 
@@ -58,30 +69,32 @@ describe('Salsa Screen', () => {
     cy.get('.rightScroll').click().get('.slider-cards').scrollTo('right')
   })
 
-  // it('should let a user search for a movie and display search results', () => {
-  //   cy
-  //     .get('form input[type=text]').type('alan@bla')
-  // })
+  it('should let a user search for a movie and display search results', () => {
+    cy.get('form input[type=text]').type('money')
+    cy.url().should('include', 'searchResults')
+    cy.get('.movies').children('a').children('.card')
+      .get('.card:first').should('have.id', '694919')
+  })
 
   it('should let a user click on a movie and see a new view of those movie details and videos', () => {
     cy.get('.movies')
       .get('.card:first').click({ multiple: true })
     cy.url().should('include', '694919')
   
-    // cy.get('.selectedMovie').children('.banner', '.banner-info', '.banner-info-title', 'Link', 'h1', '.banner-info-rating', '.rating', '.info', '.trailers')
-    // .find('img').should('have.attr', 'src').should('include', 'https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg')
+    cy.get('.selected-movie').children('.banner', '.info', '.trailers')
+      .get('.banner').children('.banner-info').children('.banner-info-title','.banner-info-rating')
+      .get('.title').contains("Money Plane")
+      .get('.trailers').children('h3', '.videos-list')
+      .get('.videos-list').children('.trailer')
   });
 
-  // it('should let a user exit the movie details page and return to a home view', () => {
-  //   cy.visit('http://localhost:3000/movies/694919')
-  //   cy.get('.selectedMovie').get('.exit').click({ multiple: true })
-  // });
- 
-  // it('should let a user click on the header page name to return to the home view', () => {
-  //   cy.get('header').
-
-  // });
-
+  it('should let a user exit the movie details page and return to a home view', () => {
+    cy.get('.movies')
+      .get('.card:first').click({ multiple: true })
+    cy.get('.selected-movie').get('.banner').get('.banner-info').get('.banner-info-title')
+      .get('.exit').click({ multiple: true })
+    cy.url().should('include', '/')
+  });
 });
 
 describe('Error page', () => {
@@ -91,26 +104,18 @@ describe('Error page', () => {
         { statusCode: 500
         }
     )
-    // cy.intercept('GET', 
-    //   'https://rancid-tomatillos.herokuapp.com/api/v2/movies694919/', 
-    //   { statusCode: 500
-    //   }  
-    // )
-    // cy.intercept('GET', 
-    // 'https://rancid-tomatillos.herokuapp.com/api/v2/movies694919/videos', 
-    //   { statusCode: 500
-    //   }
-    // // )
     cy.visit('http://localhost:3000')
     cy.get('.error')
       .get('p').contains('Sorry we\'re having trouble loading the movies, have some chips and try reloading!')  
   });
 
+
+
   it('should show a page not found redirect for a bad path', () => {
     cy.visit('http://localhost:3000/nowhere')
-    // cy
-        
-    //   .get('.notFound').children('p', 'Link')
-    //   .get('Link').click({ multiple: true })
+    cy.url().should('contain', '404')
+    cy.get('.notFound').children('p', 'a')
+      .get('.redirect-home').click()
+    cy.url().should('contain', '/')
   });
 });
